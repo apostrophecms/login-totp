e65b869c4a7ae1f66e724a72fe75827581ca7fbf
Jed <vjeudy@protonmail.com>
2022-01-25 23:23:08 +0100

    adds totp vue component

---
+++ ui/apos/components/AposTotp.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>My Totp Modal</h3>
+</template>
+
+<script>
+
+export default {
+  emits: [ 'done', 'block' ],
+  props: {
+    sitekey: String
+  },
+  data() {
+    return {
+      token: null
+    };
+  },
+}
+</script>
+<style lang='scss'></style>
