96136346174b0aca1707f1005ef14a301db1e7e6
Jed <vjeudy@protonmail.com>
2022-01-27 17:43:22 +0100

    configures props and verify methods

--- index.js
+++ index.js
@@ -1,29 +1,73 @@
-const { createHash } = require('crypto');
-const base32 = require('base32');
+const { randomBytes } = require('crypto');
+const base32 = require('thirty-two');
+const totp = require('totp-generator');
 
 module.exports = {
   improve: '@apostrophecms/login',
+  init (self, { totp }) {
+    if (!totp.secret) {
+      self.apos.util.warn('You should provide a secret of a length of 10 characters in the login module\'s config.');
+    } else if (totp.secret.length < 10) {
+      self.apos.util.warn('Your secret should be 10 characters length.');
+    }
+  },
   requirements(self) {
-
     return {
       add: {
         AposTotp: {
           phase: 'afterPasswordVerified',
           async props(req, user) {
-            if (!user.totp) {
-              const hash = createHash('sha512').update(user._id).digest('hex');
+            if (!user.totp || !user.totp.activated) {
+              const validSecret = self.getSecret();
+              const hash = randomBytes(validSecret ? 5 : 10).toString('hex');
+              const token = self.generateToken(hash, validSecret);
 
-              const token = base32.encode(hash);
+              user.totp = {
+                hash,
+                activated: false
+              };
 
+              try {
+                await self.apos.user
+                  .update(req, user, { permissions: false });
+              } catch (err) {
+                throw self.apos.error('unprocessable', req.t('AposTotp:updateError'));
+              }
+
+              return {
+                token,
+                userId: user._id,
+                username: user.username
+              };
             }
 
             return {};
           },
           async verify(req, user) {
-            throw self.apos.error('invalid', req.t('AposTotp:missingConfig'));
-            // if (!req.body.requirements.AposRecaptcha) {
-            // }
+            const code = req.body.requirements.AposTotp;
 
+            if (!code) {
+              throw self.apos.error('invalid', req.t('AposTotp:invalidToken'));
+            }
+
+            const userToken = self.generateToken(user.totp.hash, self.getSecret());
+
+            const totpToken = totp(userToken);
+
+            if (totpToken !== code) {
+              throw self.apos.error('invalid', req.t('AposTotp:invalidToken'));
+            }
+
+            if (!user.totp.activated) {
+              user.totp.activated = true;
+
+              try {
+                await self.apos.user
+                  .update(req, user, { permissions: false });
+              } catch (err) {
+                throw self.apos.error('unprocessable', req.t('AposTotp:updateError'));
+              }
+            }
           }
         }
       }
@@ -31,8 +75,17 @@ module.exports = {
   },
   methods(self) {
     return {
-      async checkTotp (req, token) {
+      generateToken (hash, secret) {
+        const formattedSecret = secret
+          ? secret.substring(0, 10)
+          : '';
 
+        return base32.encode(hash + formattedSecret).toString();
+      },
+      getSecret () {
+        const { secret } = self.options.totp;
+
+        return secret && secret.length >= 10 && secret;
       }
     };
   }
